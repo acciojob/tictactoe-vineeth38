2a0abf9da533a037e0494637b3ce9665825d2f63
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-06-14 22:41:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,78 @@
 //your JS code here. If required.
-let player1, player2, currentPlayer, moves;
-    const winningCombos = [
-      [1, 2, 3], [4, 5, 6], [7, 8, 9],
-      [1, 4, 7], [2, 5, 8], [3, 6, 9],
-      [1, 5, 9], [3, 5, 7]
-    ];
-    const cells = document.querySelectorAll('.cell');
-    const message = document.querySelector('.message');
-    const board = document.getElementById('board');
-    
-    function startGame() {
-      player1 = document.getElementById('player-1').value;
-      player2 = document.getElementById('player-2').value;
-      currentPlayer = player1;
-      moves = { [player1]: [], [player2]: [] };
-      message.innerHTML = `${currentPlayer}, you're up!`;
-      board.style.display = 'block';
-    }
-    
-    function play(id) {
-      const cell = document.getElementById(id);
-      if (!cell.innerHTML) {
-        cell.innerHTML = currentPlayer === player1 ? 'X' : 'O';
-        moves[currentPlayer].push(id);
-		 if (checkWin()) {
-          message.innerHTML = `${currentPlayer} congratulations you won!`;
-          for (const cell of cells) {
-            cell.onclick = null;
-          }
-          return;
+ var currentPlayer = 1;
+      var board = document.getElementById("board");
+      var cells = document.getElementsByClassName("cell");
+      var message = document.getElementById("message");
+      var player1Name, player2Name;
+
+      function startGame() {
+        player1Name = document.getElementById("player-1").value;
+        player2Name = document.getElementById("player-2").value;
+
+        if (player1Name && player2Name) {
+          document.getElementById("submit").disabled = true;
+          createBoard();
+          displayMessage(player1Name + ", you're up!");
         }
+      }
+function createBoard() {
+        for (var i = 1; i <= 9; i++) {
+          var cell = document.createElement("div");
+          cell.classList.add("cell");
+          cell.setAttribute("id", i);
+          cell.addEventListener("click", cellClicked);
+          board.appendChild(cell);
+        }
+      }
+
+      function cellClicked() {
+        if (!this.textContent) {
+          if (currentPlayer === 1) {
+            this.textContent = "X";
+            displayMessage(player2Name + ", you're up!");
+            currentPlayer = 2;
+          } else {
+            this.textContent = "O";
+            displayMessage(player1Name + ", you're up!");
+            currentPlayer = 1;
+          }
+          checkWin();
+        }
+      }
+function checkWin() {
+        var winningCombos = [
+          [1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+          [1, 4, 7],
+          [2, 5, 8],
+          [3, 6, 9],
+          [1, 5, 9],
+          [3, 5, 7],
+        ];
+
+        for (var i = 0; i < winningCombos.length; i++) {
+          var combo = winningCombos[i];
+          var cell1 = document.getElementById(combo[0]).textContent;
+          var cell2 = document.getElementById(combo[1]).textContent;
+          var cell3 = document.getElementById(combo[2]).textContent;
+
+          if (cell1 && cell1 === cell2 && cell1 === cell3) {
+			  var winner = cell1 === "X" ? player1Name : player2Name;
+            displayMessage(winner + ", congratulations, you won!");
+            disableCells();
+            break;
+			 }
+        }
+      }
+
+      function disableCells() {
+        for (var i = 0; i < cells.length; i++) {
+          cells[i].removeEventListener("click", cellClicked);
+        }
+      }
+
+      function displayMessage(msg) {
+        message.textContent = msg;
+      }  
+       
